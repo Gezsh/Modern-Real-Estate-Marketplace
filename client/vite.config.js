ad586fc33fa8c6ad68369e5699dfe78c54e282dd
Gezsh <teramedewgezhagn@gmail.com>
2024-03-04 11:27:31 +0300

    bcrypt

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':{
-        target:'https://modern-real-estate-marketplace-1.onrender.com',
-        secure:false,
+  // server:{
+  //   proxy:{
+  //     '/api':{
+  //       target:'https://modern-real-estate-marketplace-1.onrender.com',
+  //       secure:false,
         
-      },
-    },
-  },
+  //     },
+  //   },
+  // },
   plugins: [react()],
 })
 
