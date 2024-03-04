e40f7c2214541cfa0d572ef5ece2ebb7b81b22f3
Gezsh <teramedewgezhagn@gmail.com>
2024-03-04 09:16:43 +0300

    proxy

--- api/index.js
+++ api/index.js
@@ -8,28 +8,28 @@ const listingRouter=require('./routes/listing.route')
 require('dotenv').config()
 
 const app=express();
-
-// const __dirname=path.resolve();
-const corsOptions = {
-    credentials:true,
-    origin: 'https://realestatesgezsh.netlify.app',
-    credentials: true, // Enable credentials (cookies)
-  };
-
-
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-  });
-
 const cors=require('cors')
+// const __dirname=path.resolve();
+// const corsOptions = {
+//     credentials:true,
+//     origin: 'https://realestatesgezsh.netlify.app',
+//     credentials: true, // Enable credentials (cookies)
+//   };
+
+
+//   app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     next();
+//   });
+
+
 app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URL).then(()=>console.log("connected to database")).catch((err)=>console.log(err));
 
 
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(cookieParser())
 
 app.listen (3000,()=>{
